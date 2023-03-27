26201120e04ffbb93967289f1a457e66794d97b9
Egor <Kapitonov281087@mail.ru>
2023-03-27 13:11:40 +0900

    HW_4 full

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -7,15 +7,22 @@ import pytest
 @pytest.fixture
 def name():
     return "iphone 14pro"
+
+
 @pytest.fixture
 def price():
     return 120000.0
+
+
 @pytest.fixture
 def quantity():
     return 10
+
+
 @pytest.fixture
 def number_of_sim():
     return 2
+
 
 def test_Phone(name, price, quantity, number_of_sim):
     product = Phone(name, price, quantity, number_of_sim)
@@ -26,16 +33,19 @@ def test_Phone(name, price, quantity, number_of_sim):
     product.apply_discount()
     assert product.price == 102000
 
+
 def test_name():
     """тестируем name сеттер"""
     assert Phone.all[0].name == "iphone 14pro"
     Phone.all[0].name = "Телефон"
     assert Phone.all[0].name == "Телефон"
 
+
 def test_str():
     """тестируем __str__"""
     assert repr(Phone.all[0]) == "Phone('Телефон', 102000.0, 10, 2)"
     assert str(Phone.all[0]) == 'Телефон'
+
 
 def test_add():
     """
